a9596e35d2d8131f373970fd07bdd3a176239f4c
Dmitriy Beglov <ditransler@gmail.com>
2017-12-17 15:00:46 +0400

    Finished Lesson: Loading data into state onClick

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component {
   constructor() {
     super();
 
     this.addFish = this.addFish.bind(this);
+    this.loadSamples = this.loadSamples.bind(this);
 
     // initial state
     this.state = {
@@ -26,6 +28,12 @@ class App extends React.Component {
     this.setState({ fishes });
   }
 
+  loadSamples() {
+    this.setState({
+      fishes: sampleFishes
+    });
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -33,7 +41,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market"/>
         </div>
         <Order/>
-        <Inventory addFish={ this.addFish }/>
+        <Inventory addFish={ this.addFish } loadSamples={ this.loadSamples }/>
       </div>
     );
   }
