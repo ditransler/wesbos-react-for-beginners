67a73346019545df8e25bca95defb175d0e09602
Dmitriy Beglov <ditransler@gmail.com>
2017-12-17 14:45:36 +0400

    Finished Lesson: Understanding State

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,28 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.addFish = this.addFish.bind(this);
+
+    // initial state
+    this.state = {
+      fishes: {},
+      order: {}
+    };
+  }
+
+  addFish(fish) {
+    // Update our state
+    const fishes = { ...this.state.fishes };
+    // Add our new fish
+    const timestamp = Date.now();
+    fishes[`fish-${timestamp}`] = fish;
+    // Set state
+    this.setState({ fishes });
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -11,7 +33,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market"/>
         </div>
         <Order/>
-        <Inventory/>
+        <Inventory addFish={ this.addFish }/>
       </div>
     );
   }
